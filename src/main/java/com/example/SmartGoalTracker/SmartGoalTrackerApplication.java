602510c8153d65799fdbce51336e271d3b54220f
Iliya Raev <insomniq4@abv.bg>
2025-11-18 20:55:29 +0200

    Initial commit

---
+++ src/main/java/com/example/SmartGoalTracker/SmartGoalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartGoalTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGoalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGoalTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SmartGoalTracker/SmartGoalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartGoalTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGoalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
