3a4cced6a118ab7a0ebe1719fdbbdef6d28bb802
Iliya Raev <insomniq4@abv.bg>
2025-11-19 20:34:43 +0200

    Add DTOs for authentication

---
+++ src/main/java/com/example/SmartGoalTracker/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.example.SmartGoalTracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
+
