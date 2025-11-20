c7ba8cce07a47920cbbc1106326908ad7fae411b
Iliya Raev <insomniq4@abv.bg>
2025-11-20 18:30:14 +0200

    Create goals table

--- src/main/java/com/example/SmartGoalTracker/model/Goal.java
+++ src/main/java/com/example/SmartGoalTracker/model/Goal.java
@@ -1,4 +1,35 @@
 package com.example.SmartGoalTracker.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "goals")
 public class Goal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    private LocalDateTime target_date;
+
+    private final LocalDateTime created_at = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
 }
