7dc2eb1b71f7ed3d8a382a59254dfe5a888d4645
Iliya Raev <insomniq4@abv.bg>
2025-11-21 18:53:58 +0200

    Add fields in SubGoal class

--- src/main/java/com/example/SmartGoalTracker/model/SubGoal.java
+++ src/main/java/com/example/SmartGoalTracker/model/SubGoal.java
@@ -1,4 +1,31 @@
 package com.example.SmartGoalTracker.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "sub_goals")
 public class SubGoal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    private Boolean isCompleted = false;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "goal_id", nullable = false)
+    private Goal goal;
 }
