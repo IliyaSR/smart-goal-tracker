922cacedb6d804f7bcb4cf4d2bed6e1fb7d34bcc
Iliya Raev <insomniq4@abv.bg>
2025-11-23 13:05:26 +0200

    Create GoalService interface

---
+++ src/main/java/com/example/SmartGoalTracker/service/GoalService.java
@@ -0,0 +1,9 @@
+package com.example.SmartGoalTracker.service;
+
+import com.example.SmartGoalTracker.model.Goal;
+
+import java.util.List;
+
+public interface GoalService {
+    public List<Goal> getAllGoals();
+}
