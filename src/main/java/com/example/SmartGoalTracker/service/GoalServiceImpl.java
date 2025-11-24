a01b1f8a5461710102926821d1856973c9af0c1c
Iliya Raev <insomniq4@abv.bg>
2025-11-24 11:18:14 +0200

    Create check if for empty goals list

--- src/main/java/com/example/SmartGoalTracker/service/GoalServiceImpl.java
+++ src/main/java/com/example/SmartGoalTracker/service/GoalServiceImpl.java
@@ -2,12 +2,14 @@ package com.example.SmartGoalTracker.service;
 
 import com.example.SmartGoalTracker.dto.GoalDto;
 import com.example.SmartGoalTracker.dto.SubgoalDto;
+import com.example.SmartGoalTracker.exception.ResourceNotFoundException;
 import com.example.SmartGoalTracker.model.Goal;
 import com.example.SmartGoalTracker.repository.GoalRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class GoalServiceImpl implements GoalService {
@@ -21,7 +23,7 @@ public class GoalServiceImpl implements GoalService {
     @Override
     public List<GoalDto> getAllGoals() {
 
-        return goalRepository.findAll()
+        List<GoalDto> goals =  goalRepository.findAll()
                 .stream()
                 .map(
                         goal ->
@@ -44,5 +46,12 @@ public class GoalServiceImpl implements GoalService {
                                                 ).toList()
                                 )
                 ).toList();
+
+        if(goals.isEmpty()){
+            throw new ResourceNotFoundException("No goals found!");
+        }else{
+            return goals;
+        }
+
     }
 }
