c1114c9c113c01b9bf36ac6beb0173c648146ede
Iliya Raev <insomniq4@abv.bg>
2025-11-24 10:57:48 +0200

    Create goal controller

--- src/main/java/com/example/SmartGoalTracker/controller/GoalController.java
+++ src/main/java/com/example/SmartGoalTracker/controller/GoalController.java
@@ -1,4 +1,25 @@
 package com.example.SmartGoalTracker.controller;
 
+import com.example.SmartGoalTracker.dto.GoalDto;
+import com.example.SmartGoalTracker.service.GoalServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/goals")
 public class GoalController {
+
+    private final GoalServiceImpl goalService;
+
+    GoalController(GoalServiceImpl goalService){
+        this.goalService = goalService;
+    }
+
+    @GetMapping
+    public List<GoalDto> getAllGoals(){
+        return goalService.getAllGoals();
+    }
 }
