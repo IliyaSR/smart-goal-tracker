e6dec6bbec8c4bab302bf307ca60689b3b0130e0
Iliya Raev <insomniq4@abv.bg>
2025-11-28 12:52:06 +0200

    Implement method from GoalService

--- src/main/java/com/example/SmartGoalTracker/service/GoalServiceImpl.java
+++ src/main/java/com/example/SmartGoalTracker/service/GoalServiceImpl.java
@@ -2,9 +2,12 @@ package com.example.SmartGoalTracker.service;
 
 import com.example.SmartGoalTracker.dto.GoalRequest;
 import com.example.SmartGoalTracker.dto.GoalResponse;
-import com.example.SmartGoalTracker.exception.ResourceNotFoundException;
 import com.example.SmartGoalTracker.model.Goal;
+import com.example.SmartGoalTracker.model.User;
 import com.example.SmartGoalTracker.repository.GoalRepository;
+import com.example.SmartGoalTracker.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,9 +16,11 @@ import java.util.List;
 public class GoalServiceImpl implements GoalService {
 
     private final GoalRepository goalRepository;
+    private final UserRepository userRepository;
 
-    GoalServiceImpl(GoalRepository goalRepository) {
+    GoalServiceImpl(GoalRepository goalRepository, UserRepository userRepository) {
         this.goalRepository = goalRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -26,6 +31,26 @@ public class GoalServiceImpl implements GoalService {
         return goals.stream()
                 .map(this::mapToResponse)
                 .toList();
+    }
+
+    @Override
+    public GoalResponse createGoal(GoalRequest goalRequest) {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+
+        Goal goal = Goal.builder()
+                .user(user)
+                .title(goalRequest.getTitle())
+                .description(goalRequest.getDescription())
+                .targetDate(goalRequest.getTargetDate())
+                .build();
+
+        goalRepository.save(goal);
+
+        return mapToResponse(goal);
     }
 
     public GoalResponse mapToResponse(Goal goal) {
