1e42c0945907f8cb83120a21a3b1bf89f73aaf02
Iliya Raev <insomniq4@abv.bg>
2025-11-29 17:42:17 +0200

    Add handlingException]

--- src/main/java/com/example/SmartGoalTracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/SmartGoalTracker/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.SmartGoalTracker.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +12,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleNotValidArgument(MethodArgumentNotValidException ex) {
+
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .findFirst()
+                .orElse("Invalid input");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
 }
