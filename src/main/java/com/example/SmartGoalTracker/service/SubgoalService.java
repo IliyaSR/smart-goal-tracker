5d850cbc9eccea4a451bb88eb52b4be0bcdb6e6d
Iliya Raev <insomniq4@abv.bg>
2025-12-03 17:15:21 +0200

    Create updateGoal method in SubgoalService

--- src/main/java/com/example/SmartGoalTracker/service/SubgoalService.java
+++ src/main/java/com/example/SmartGoalTracker/service/SubgoalService.java
@@ -6,4 +6,5 @@ import com.example.SmartGoalTracker.dto.SubgoalResponse;
 
 public interface SubgoalService {
     public SubgoalResponse createSubgoal(Long goalId, SubgoalRequest subgoalRequest);
+    public SubgoalResponse updateSubgoal(Long id);
 }
