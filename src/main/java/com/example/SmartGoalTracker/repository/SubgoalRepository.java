3a6b534e1e5e8db2080e0c1d8fd4baae53cb9ce6
Iliya Raev <insomniq4@abv.bg>
2025-12-03 16:58:56 +0200

    Create Subgoal Repository

--- src/main/java/com/example/SmartGoalTracker/repository/SubgoalRepository.java
+++ src/main/java/com/example/SmartGoalTracker/repository/SubgoalRepository.java
@@ -1,4 +1,9 @@
 package com.example.SmartGoalTracker.repository;
 
-public interface SubgoalRepository {
+import com.example.SmartGoalTracker.model.Subgoal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubgoalRepository extends JpaRepository<Subgoal, Long> {
 }
