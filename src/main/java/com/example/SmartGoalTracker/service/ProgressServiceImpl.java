16f417e3f99d53384f90fd42ac14e445fe413992
Iliya Raev <insomniq4@abv.bg>
2025-12-03 18:10:52 +0200

    Create ProgressServiceImpl and implement createProgress method

--- src/main/java/com/example/SmartGoalTracker/service/ProgressServiceImpl.java
+++ src/main/java/com/example/SmartGoalTracker/service/ProgressServiceImpl.java
@@ -1,4 +1,47 @@
 package com.example.SmartGoalTracker.service;
 
-public class ProgressServiceImpl {
+import com.example.SmartGoalTracker.dto.ProgressRequest;
+import com.example.SmartGoalTracker.dto.ProgressResponse;
+import com.example.SmartGoalTracker.dto.SubgoalRequest;
+import com.example.SmartGoalTracker.model.ProgressLog;
+import com.example.SmartGoalTracker.model.Subgoal;
+import com.example.SmartGoalTracker.repository.ProgressRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProgressServiceImpl implements ProgressService{
+
+    private final ProgressRepository progressRepository;
+    private final SubgoalServiceImpl subgoalService;
+
+    public ProgressServiceImpl(ProgressRepository progressRepository, SubgoalServiceImpl subgoalService) {
+        this.progressRepository = progressRepository;
+        this.subgoalService = subgoalService;
+    }
+
+    @Override
+    public ProgressResponse createProgress(Long subGoalId, ProgressRequest progressRequest) {
+
+        Subgoal subgoal = subgoalService.getSubgoal(subGoalId);
+
+        ProgressLog progressLog =
+                ProgressLog.builder()
+                        .subgoal(subgoal)
+                        .progressPercent(progressRequest.getProgressPercent())
+                        .build();
+
+        progressRepository.save(progressLog);
+
+        return mapToResponse(progressLog);
+
+    }
+
+    private ProgressResponse mapToResponse(ProgressLog progressLog){
+        return ProgressResponse.builder()
+                .id(progressLog.getId())
+                .subgoalId(progressLog.getSubgoal().getId())
+                .progressDate(progressLog.getProgressDate())
+                .progressPercent(progressLog.getProgressPercent())
+                .build();
+    }
 }
